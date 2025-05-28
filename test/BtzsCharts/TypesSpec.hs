12b82f020d33af8959c942c8655b9ff6a56c47ca
Marco Minutoli <mminutoli@gmail.com>
2025-05-28 00:16:21 -0700

    Add skeleton for the tests.

---
+++ test/BtzsCharts/TypesSpec.hs
@@ -0,0 +1,20 @@
+{-|
+Module      : BtzsCharts.TypesSpec
+Description : Tests for the module BtzsCharts.Types.
+Copyright   : (c) Marco Minutoli, 2025
+
+License     : BSD-3
+Maintainer  : Marco Minutoli <mminutoli@gmail.com>
+Stability   : experimental
+Portability : POSIX
+
+This module tests the properties and functionalities of the BtzsCharts.Types.
+-}
+
+module BtzsCharts.TypesSpec(btzsChartsTypesTests) where
+
+import Test.Tasty
+import BtzsCharts.Types
+
+btzsChartsTypesTests :: TestTree
+btzsChartsTypesTests = testGroup "Tests for BtzsCharts.Types" []
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,4 +1,23 @@
+{-|
+Module      : Main
+Description : Tests for the module BtzsCharts package.
+Copyright   : (c) Marco Minutoli, 2025
+
+License     : BSD-3
+Maintainer  : Marco Minutoli <mminutoli@gmail.com>
+Stability   : experimental
+Portability : POSIX
+
+This module tests the properties and functionalities of the BtzsCharts package.
+-}
 module Main (main) where
 
+import Test.Tasty
+
+import BtzsCharts.TypesSpec(btzsChartsTypesTests)
+
+tests :: TestTree
+tests = testGroup "Tests" [btzsChartsTypesTests]
+
 main :: IO ()
-main = putStrLn ("Tests not Yet Implemented")
+main = defaultMain tests
