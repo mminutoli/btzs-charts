0854b255bc02c3c5b3042d1d7fd933bf8a2f53b7
Marco Minutoli <mminutoli@gmail.com>
2025-06-24 14:26:45 +0200

    Implement functions to find IDmin and IDmax on HDCurve.

--- src/BtzsCharts/HDCurveFitting.hs
+++ src/BtzsCharts/HDCurveFitting.hs
@@ -12,11 +12,19 @@ Portability : POSIX
 
 module BtzsCharts.HDCurveFitting (
   HDCurve(..),
-  fitCurves
+  fitCurves,
+  basePlusFog,
+  findPoint,
+  avgGradient,
+  idMinTarget,
+  findIDmin,
+  findIDmax
   ) where
 
+import Control.Monad.Reader
 import BtzsCharts.Types
 import Data.Vector.Storable as VS
+import Data.Maybe
 import Numeric.GSL.Interpolation (evaluateV, InterpolationMethod(Linear))
 
 import qualified Data.Map as M
@@ -75,3 +83,86 @@ fitCurves stepWedge materialTest =
   where
     experiments = (M.toList . results) materialTest
     stepWedgeDensities = densities stepWedge
+
+-- | The value of based plus fog as read from the sensitometer.
+--
+-- Arguments:
+-- * @curve@: The HD-Curve data fit from the sensitometric measurements.
+basePlusFog :: HDCurve -> Density
+basePlusFog = VS.last . outputDensity
+
+-- | Find the first point on the HD-Curve having at least the target density.
+--
+-- The function returns the first point on the HD-Curve with at least target
+-- density above `basePlusFog` or the highest recorded density otherwise.
+-- While not perfect, this solution avoids dealing with Maybe everywhere and
+-- fits well the analysis that we need to perform on the curves. Nevertheless
+-- if anyone has better ideas, I am open to suggestions.
+--
+-- Arguments:
+-- * @target@: The target above base+fog to determine the speed point.
+-- * @curve@: The HD-Curve data fit from the sensitometric measurements.
+findPoint :: Density -> HDCurve -> (Int, Density, Density)
+findPoint target curve = (pos, e, d)
+  where
+    targetDensity = basePlusFog curve + target
+    idx = VS.findIndexR (>= targetDensity) (outputDensity curve)
+    pos = fromMaybe 0 idx
+    e = relativeLogExposure curve VS.! pos
+    d = outputDensity curve VS.! pos
+
+-- | Compute the average gradient of a curve.
+--
+-- We compute the average gradient of the curve between IDmin and
+-- IDmax or the highest density recorded. This is not necessarily correct as
+-- the curve might start to shoulder.  However, for what I have seen so far,
+-- modern films tend to have relatively straight characteristic curve. Hence,
+-- the error in computing the gradient should be relatively small.
+--
+-- Arguments:
+-- * @curve@: The HD-Curve data fit from the sensitometric measurements.
+avgGradient :: HDCurve -> ProcessConfM Double
+avgGradient curve = do
+  (_, x, y) <- findIDmin curve
+  flareFactor <- asks flareCompensationFactor
+  si <- asks scaleIndex
+  let target = y + si * flareFactor
+  let (_, x', y') = findPoint target curve
+  return ((y' - y) / (x' - x))
+
+-- | The IDmin target according to the process configuration.
+idMinTarget :: ProcessConfM Density
+idMinTarget = do
+  g <- asks standardAvgGradient
+  f <- asks speedPointFactor
+  flareFactor <- asks flareCompensationFactor
+  return ((g / f) * flareFactor)
+
+-- | Find the coordinates of the IDmin on the HD-Curve.
+--
+-- Arguments:
+-- * @curve@: The HD-Curve data fit from the sensitometric measurements.
+findIDmin :: HDCurve -> ProcessConfM (Int, Density, Density)
+findIDmin curve = do
+  target <- idMinTarget
+  return (findPoint target curve)
+
+-- | Find the coordinates of the IDmax on the HD-Curve.
+--
+-- IDmax is either found on the HD-Curve or projected using the average
+-- gradient.
+--
+-- Arguments:
+-- * @curve@: The HD-Curve data fit from the sensitometric measurements.
+findIDmax :: HDCurve -> ProcessConfM (Int, Density, Density)
+findIDmax curve = do
+  (_, _, d) <- findIDmin curve
+  flareFactor <- asks flareCompensationFactor
+  si <- asks scaleIndex
+  let target = d + si * flareFactor
+  let (p, x, y) = findPoint target curve
+  if y == target
+    then return (p, x, y)
+    else do
+      gradient <- avgGradient curve
+      return (-1, x/gradient, target)
