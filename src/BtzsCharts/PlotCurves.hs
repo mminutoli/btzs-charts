515ecebf78b3c2605401b29334419be27b2bad7b
Marco Minutoli <mminutoli@gmail.com>
2025-06-28 10:38:21 +0200

    Add color selection for the different curves in HD plots.

--- src/BtzsCharts/PlotCurves.hs
+++ src/BtzsCharts/PlotCurves.hs
@@ -16,23 +16,26 @@ module BtzsCharts.PlotCurves(
 
 import Control.Lens
 import Data.Default.Class
+import Data.Colour(opaque)
+import Data.Colour.Palette.BrewerSet
 import BtzsCharts.HDCurveFitting (HDCurve(..))
 import Graphics.Rendering.Chart
-import Graphics.Rendering.Chart.Backend.Diagrams (renderableToFile)
+import Graphics.Rendering.Chart.Backend.Diagrams
 import Data.Vector.Storable(toList)
 import BtzsCharts.Types (Density)
 
 plotHDCurves :: [HDCurve] -> Layout Density Density
 plotHDCurves curves = layout
   where
-    hdLine c =
+    hdLine i c =
       plot_lines_title .~ hdCurveLabel c
+      $ plot_lines_style . line_color .~ color i
       $ plot_lines_values .~ [zip (toList $ relativeLogExposure c) (toList $ outputDensity c)]
       $ def
     layout = layout_title .~ "HD-Curve"
-      $ layout_plots .~ Prelude.map (toPlot . hdLine) curves
+      $ layout_plots .~ (Prelude.map toPlot $ Prelude.zipWith hdLine [0..] curves)
       $ def
-
+    color i = opaque $ brewerSet Set1 (length curves) !! i
 
 saveToFile :: ToRenderable p => p -> FilePath -> IO()
 saveToFile layout path = do
